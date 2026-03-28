ee5f3d0d45c7a44514d8b61fb0a698ee984727ec
DESKTOP-JJVRQN7\michal <niezgmi9164_aehit@students.vizja.pl>
2026-03-28 17:19:22 +0100

    LAB2_POPRAWIONE_DRUGIRAZ

--- Python/django_aplikacja_ankietowa/mysite/polls/views.py
+++ Python/django_aplikacja_ankietowa/mysite/polls/views.py
@@ -7,40 +7,26 @@ from django.views import generic
 
 from .models import Choice, Question
 
+
 class IndexView(generic.ListView):
+
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
-def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
-        :5
-    ]
+    def get_queryset(self):
+
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by("-pub_date")[:5]
+
+
 class DetailView(generic.DetailView):
-    ...
+    
+    model = Question
+    template_name = "polls/detail.html"
 
     def get_queryset(self):
-        
-        """
-        Excludes any questions that aren't published yet.
-        """
         return Question.objects.filter(pub_date__lte=timezone.now())
-
-def index(request):
-    latest_question_list = Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by("-pub_date")[:5]
-
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
@@ -63,9 +49,8 @@ def vote(request, question_id):
             },
         )
     else:
-        selected_choice.votes = F("votes") + 1
+        selected_choice.votes =  F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(
             reverse("polls:results", args=(question.id,))
         )
-        
